e5c0af94a4286bab3b96252ad764803adafdc43f
Adam Naylor <adamnaylor@gmail.com>
2015-10-27 15:53:36 +0000

    Fixed unit tests

--- AJN.Gorman.Core.UnitTests/MapServiceTests.cs
+++ AJN.Gorman.Core.UnitTests/MapServiceTests.cs
@@ -1,12 +1,12 @@
 ﻿
 
-using System.Data.Entity;
-using AJN.Gorman.API.Core.Services;
-using AJN.Gorman.Domain;
-using Moq;
 
 namespace AJN.Gorman.Core.UnitTests
 {
+    using API.Core.Services;
+    using Domain;
+    using Microsoft.Data.Entity;
+    using Moq;
     using NUnit.Framework;
 
     [TestFixture]
@@ -16,13 +16,13 @@ namespace AJN.Gorman.Core.UnitTests
         public void Add_WithNewMap_AddsToDatastore()
         {
             var fakeContext = new Mock<IEntitiesContext>();
-            var fakeMapSet = new Mock<IDbSet<Map>>();
+            var fakeMapSet = new Mock<DbSet<Map>>();
             fakeContext.Setup(c => c.Maps).Returns(fakeMapSet.Object);
             var mapService = new MapService(fakeContext.Object);
 
             mapService.Add(new Map());
 
-            fakeMapSet.Verify(c => c.Add(It.IsAny<Map>()));
+            fakeMapSet.Verify(c => c.Add(It.IsAny<Map>(), GraphBehavior.IncludeDependents)); //have to include optional argument
             fakeContext.Verify(c=>c.SaveChanges());
         }
     }
